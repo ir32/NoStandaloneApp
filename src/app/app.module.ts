f4049a04fcb601882371c52eb988e4d20aeb8108
ir32 <alamirfan651@gmail.com>
2024-08-20 12:56:13 +0530

    api data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,19 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductTableComponent } from './product-table/product-table.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductTableComponent
+    ProductTableComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-table',
   templateUrl: './product-table.component.html',
-  styleUrl: './product-table.component.css'
+  styleUrls: ['./product-table.component.css']
 })
-export class ProductTableComponent {
+export class ProductTableComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe(data => {
+      this.products = data;
+      console.log('Product data:', this.products); // Log the data to the console
+    });
+  }
 }
