351013aa9bf1c668c09712f049bd6f964feadef8
ir32 <alamirfan651@gmail.com>
2024-08-24 19:03:36 +0530

    increment and decrement

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -7,14 +8,27 @@ import { CartService } from '../services/cart.service';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  // take blank array
   carts: any[] = [];
 
   constructor(private cartService: CartService) {}
 
   ngOnInit(): void {
     this.cartService.fetchData().subscribe((data) => {
-      this.carts = data; // Store the fetched data in the `carts` array
-      console.log(data);
+      this.carts = data;
+      console.log(this.carts);
     });
   }
+
+  increment(product: any): void {
+    product.quantity++;
+  }
+
+  decrement(product: any): void {
+    if (product.quantity > 0) {
+      product.quantity--;
+    }
+  }
+
+  
 }
